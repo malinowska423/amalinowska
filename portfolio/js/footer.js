43b223cdda086f64392a05acbcf9ef3c91cc6fc3
malinowska423 <amalinowska423@gmail.com>
2019-10-12 20:52:39 +0200

    moved contact section to js function

--- portfolio/js/footer.js
+++ portfolio/js/footer.js
@@ -1,3 +1,25 @@
 function loadFooter() {
-    document.write("<p>2019 &copy; Aleksandra Malinowska | All rights reserved</p>")
+    document.write('<div class="center"><p>2019 &copy; Aleksandra Malinowska | All rights reserved</p></div>')
+}
+
+function loadContact() {
+    document.write(
+        '        <section class="center main-section section-col-5">\n' +
+        '            <h2 class="section-title">Skontaktuj się</h2>\n' +
+        '            <div class="column social-icon">\n' +
+        '                <a href="https://www.linkedin.com/in/amalinowska423/" target="_blank"><img src="img/in.png" alt="Linked in"></a>\n' +
+        '            </div>\n' +
+        '            <div class="column social-icon">\n' +
+        '                <a href="https://github.com/malinowska423" target="_blank"><img src="img/git.png" alt="Github"></a>\n' +
+        '            </div>\n' +
+        '            <div class="column social-icon">\n' +
+        '                <a href="https://www.instagram.com/olinnn10/" target="_blank"><img src="img/insta.png" alt="Instagram"></a>\n' +
+        '            </div>\n' +
+        '            <div class="column social-icon">\n' +
+        '                <a href="mailto:am@amalinowska.pl" target="_blank"><img src="img/mail.png" alt="E-mail"></a>\n' +
+        '            </div>\n' +
+        '            <div class="column social-icon">\n' +
+        '                <a href="../index.html" target="_blank"><img src="img/gallery.png" alt="Gallery"></a>\n' +
+        '            </div>\n' +
+        '        </section>')
 }
