a0cb45be75792223e6eae992c351342e3092f781
malinowska423 <amalinowska423@gmail.com>
2020-02-19 16:45:42 +0100

    navbar on all screens finished

--- src/app/modules/home/menu/menu.component.ts
+++ src/app/modules/home/menu/menu.component.ts
@@ -1,22 +1,49 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('stickyMenu') menu: ElementRef;
 
   isMenuOpened = false;
+  sticky = false;
+  width = 0;
+  menuPosition: any;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.checkWindowSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.checkWindowSize();
   }
 
   openMenu() {
     this.isMenuOpened = !this.isMenuOpened;
   }
 
+  checkWindowSize() {
+    this.width = window.innerWidth;
+    if (window.innerWidth > 1280) {
+      this.isMenuOpened = true;
+    }
+  }
+
+  ngAfterViewInit() {
+    this.menuPosition = this.menu.nativeElement.offsetTop;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  handleScroll() {
+    const windowScroll = window.pageYOffset;
+    this.sticky = windowScroll >= this.menuPosition;
+  }
 }
